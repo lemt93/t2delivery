e32f9a238e3dc0f3c94ea8cc45d34470186a994f
Tam M Le <lemt93@gmail.com>
2020-12-07 16:20:27 +0700

    Setup nginx deployment

--- aws-codedeploy/scripts/application_start.sh
+++ aws-codedeploy/scripts/application_start.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+PROJECT_NAME='t2delivery'
+SOURCE_PATH=/home/ubuntu/github/$PROJECT_NAME
+NGINX_SITE_PATH=/var/www/$PROJECT_NAME
 
 echo "Application start"
+cd $SOURCE_PATH
+yarn install
+yarn run build
+sudo cp -Rf $SOURCE_PATH/dist/. $NGINX_SITE_PATH
+sudo cp $SOURCE_PATH/aws-codedeploy/nginx/nginx.conf /etc/nginx/nginx.conf
+sudo cp $SOURCE_PATH/aws-codedeploy/nginx/$PROJECT_NAME.conf /etc/nginx/conf.d/$PROJECT_NAME.conf
+sudo service nginx restart
